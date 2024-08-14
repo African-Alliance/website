86a6614fb5f15a8bb706e7a5ac86a88331501614
Harploid <waleii19@gmail.com>
2024-08-14 10:05:28 +0100

    work in progress, Looking at icon sizes

--- src/pages/SmartKidEduPlan.js
+++ src/pages/SmartKidEduPlan.js
@@ -17,6 +17,45 @@ export default function SmartKidEducation() {
           </article>
         </div>
       </section>
+      <section>
+        <hr className="my-3 my-md-5 mx-3" style={{ backgroundColor: '#1b3271' }}></hr>
+            <div className="investmentspecial-features d-flex flex-column align-items-center">
+
+                <h3>This section highlights the benefits and features of this plan</h3>
+                <div className="features-list d-flex flex-wrap align-items-center justify-content-center w-100">
+                    <div className="col-12 col-sm-6 col-md-3 p-2 text-center">
+                        <span className="material-icons">volunteer_activism</span>
+                        <h3>Confidence</h3>
+                        <h6>Avoid loss of money</h6>
+                        <p>Easily get all plans sorted</p>
+                    </div>
+                    <div className="product col-12 col-sm-6 col-md-3 p-2 text-center">
+                        <span className="material-icons">diversity_1</span>
+                        <h3>Ease of Access</h3>
+                        <h6>Another</h6>
+                        <p></p>
+                    </div>
+                    <div className="product col-12 col-sm-6 col-md-3 p-2 text-center">
+                        <span className="material-icons">real_estate_agent</span>
+                        <h3>Achieve mortgage dreams</h3>
+                        <h6>Another</h6>
+                        <p></p>
+                    </div>
+                    <div className="product col-12 col-sm-6 col-md-4 p-2 text-center">
+                        <span className="material-icons">diversity_1</span>
+                        <h3>Peace of mind</h3>
+                        <h6>Another</h6>
+                        <p></p>
+                    </div>
+                    <div className="product col-12 col-sm-6 col-md-4 p-2 text-center">
+                        <span className="material-icons">real_estate_agent</span>
+                        <h3>And more</h3>
+                        <h6>Another</h6>
+                        <p></p>
+                    </div>
+                </div>
+              </div>
+      </section>
     </>
   );
 };
